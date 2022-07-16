caecca8ed3e1b586b24a5a579e6ab3d8637608c3
Kirya052 <kiryakonev01@mail.ru>
2022-07-16 15:48:56 +0500

    Initialize project -Add player pawn class -Add player controller class

---
+++ BeryoskiGame/Source/BeryoskiGame/BeryoskiGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeryoskiGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BERYOSKIGAME_API ABeryoskiGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ BeryoskiGame/Source/BeryoskiGame/Pawn/Controllers/PawnPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PawnPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BERYOSKIGAME_API APawnPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
